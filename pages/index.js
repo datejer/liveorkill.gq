986f79206d7776314d7a9c9482109e2c35233853
datejer <ejer.priv@wp.pl>
2020-12-31 15:54:15 +0100

    seo has decided O_O

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,33 @@ export default function Home() {
 			<Head>
 				<title>{decision.text}</title>
 				<link rel="icon" href={decision.favicon} />
+
+				<meta name="title" content={decision.text} />
+				<meta name="description" content={`he has decided: ${decision.text}`} />
+
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content="https://liveorkill.gq/" />
+				<meta property="og:title" content={decision.text} />
+				<meta
+					property="og:description"
+					content={`he has decided: ${decision.text}`}
+				/>
+				<meta
+					property="og:image"
+					content={`https://liveorkill-gq.ejer.vercel.app${decision.img}`}
+				/>
+
+				<meta property="twitter:card" content="summary_large_image" />
+				<meta property="twitter:url" content="https://liveorkill.gq/" />
+				<meta property="twitter:title" content={decision.text} />
+				<meta
+					property="twitter:description"
+					content={`he has decided: ${decision.text}`}
+				/>
+				<meta
+					property="twitter:image"
+					content={`https://liveorkill-gq.ejer.vercel.app${decision.img}`}
+				/>
 			</Head>
 			<img src={decision.img} className={styles.image} />
 		</div>
